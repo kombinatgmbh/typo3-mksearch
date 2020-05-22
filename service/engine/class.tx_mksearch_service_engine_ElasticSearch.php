9536ebd8f5cb9d26d31cb1c0996c9e32af7edb47
David Zschille <david@zschille.net>
2020-05-22 13:54:35 +0200

    Add filter for fe user group.

--- service/engine/class.tx_mksearch_service_engine_ElasticSearch.php
+++ service/engine/class.tx_mksearch_service_engine_ElasticSearch.php
@@ -194,9 +194,11 @@ class tx_mksearch_service_engine_ElasticSearch extends \Sys25\RnBase\Typo3Wrappe
      */
     protected function getElasticaQuery(array $fields, array $options)
     {
+        /* @var Query $elasticaQuery */
         $elasticaQuery = Query::create($fields['term'] ?? '');
 
         $elasticaQuery = $this->handleSorting($elasticaQuery, $options);
+        $elasticaQuery = $this->getFilterQueryForFeGroups($elasticaQuery);
 
         return $elasticaQuery;
     }
@@ -220,6 +222,21 @@ class tx_mksearch_service_engine_ElasticSearch extends \Sys25\RnBase\Typo3Wrappe
                 ]
             );
         }
+
+        return $elasticaQuery;
+    }
+
+    /**
+     * @param Query $elasticaQuery
+     *
+     * @return Query
+     */
+    private function getFilterQueryForFeGroups(Query $elasticaQuery)
+    {
+        //$userGroups = $GLOBALS['TSFE']->fe_user->groupData['uid'];
+        $groups = ['0'];
+        $term = new Elastica\Query\Terms('fe_group_mi', $groups);
+        $elasticaQuery->setPostFilter($term);
 
         return $elasticaQuery;
     }
