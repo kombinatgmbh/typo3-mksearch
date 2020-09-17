0376c506bf0832922eaa8c71a0ff357be18b9232
David Zschille <david@zschille.net>
2020-09-17 15:28:26 +0200

    Add possibility to boost fields in query.

--- service/engine/class.tx_mksearch_service_engine_ElasticSearch.php
+++ service/engine/class.tx_mksearch_service_engine_ElasticSearch.php
@@ -257,7 +257,7 @@ class tx_mksearch_service_engine_ElasticSearch extends \Sys25\RnBase\Typo3Wrappe
     private function setFields(array $fields)
     {
         if (!isset($this->config['allowedSearchFields']) || empty($this->config['allowedSearchFields'])) {
-            return ['_all'];
+            return $this->getBoostOrDefaultFields();
         }
         $allowedFields = Tx_Rnbase_Utility_Strings::trimExplode(',', $this->config['allowedSearchFields']);
 
@@ -270,7 +270,24 @@ class tx_mksearch_service_engine_ElasticSearch extends \Sys25\RnBase\Typo3Wrappe
             }
         }
 
-        return ['_all'];
+        return $this->getBoostOrDefaultFields();
+    }
+
+    /**
+     * @return array
+     */
+    private function getBoostOrDefaultFields()
+    {
+        if (!isset($this->config['filter']['boost']) || empty($this->config['filter']['boost'])) {
+            return ['_all'];
+        }
+        $fields = [];
+
+        foreach ($this->config['filter']['boost'] as $fieldName => $boost) {
+            $fields[] = "$fieldName^$boost";
+        }
+
+        return $fields;
     }
 
     /**
